9bad1cd0de1d55c3311a3f2aaa238ae964171cbf
0xamey <ameydhurgude007@gmail.com>
2023-02-11 00:51:06 +0530

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { DefaultSeo } from 'next-seo';
 
 import { config } from 'next-seo.config';
@@ -9,15 +8,11 @@ import 'styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-12 md:mx-48 lg:mx-56 xl:mx-64">
       <DefaultSeo {...config} />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
     </div>
   );
 }
